1420ede1af7cd87cc4b034a05f050b4fbb30deb4
Danilo <Danilo@DESKTOP-0MRRVL6>
2016-11-20 10:24:24 -0500

    se agrego la relacion historiaUsuario a proyecto

---
+++ AppEjemploLayout/Models/ClasesHistoriaUsuario/HistoriaUsuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AppEjemploLayout.Models.ClasesHistoriaUsuario
+{
+    [Table("HistoriasUsuario")]
+    public class HistoriaUsuario
+    {        
+        public int HistoriaUsuarioId { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese el nombre de la historia de usuario.")]
+        [Display(Name = "Nombre *:")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese el contexto de la historia de usuario.")]
+        [Display(Name = "Contexto *:")]
+        public string contexto { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese a descripción de la historia de usuario.")]
+        [Display(Name = "Descripción : ")]
+        public string descripcion { get; set; }
+
+        [Display(Name = "Prioridad : ")]
+        public int prioridad { get; set; }
+
+        [Display(Name = "Esfuerzo : ")]
+        public int ezfuerzo { get; set; }
+
+    }
+}
--- AppEjemploLayout/Models/ClasesProyecto/Proyecto.cs
+++ AppEjemploLayout/Models/ClasesProyecto/Proyecto.cs
@@ -8,30 +8,35 @@ using System.Web;
 
 namespace AppEjemploLayout.Models.ClasesProyecto
 {
+    [Table ("Productos")]
     public class Proyecto
     {
-       public int ProyectoId { get; set; }
+        public int ProyectoId { get; set; }
 
-        [Required(ErrorMessage = "Por favor ingrese el nombre del proyecto")]
-        [Display(Name ="Nombre del proyecto*:")]
+        [Required(ErrorMessage = "Por favor ingrese el nombre del proyecto.")]
+        [Display(Name ="Nombre del proyecto * : ")]
         public string nombreProyecto { get; set; }
 
         
-        [Display(Name = "Descripcion del proyecto:")]
+        [Display(Name = "Descripcion del proyecto : ")]
         public string descripcionProyecto { get; set; }
 
-        [Display(Name = "Fecha de inicio de proyecto:")]
+        [Display(Name = "Fecha de inicio de proyecto : ")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime fechaInicioProyecto { get; set; }
 
-        [Display(Name = "Fecha de finalizacion del proyecto:")]
+        [Display(Name = "Fecha de finalizacion del proyecto : ")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime fechaFinalizacionProyecto { get; set; }
 
-        [Display(Name ="Estado del proyecto:")]
+        [Display(Name ="Estado del proyecto : ")]
         public string estadoProyecto { get; set; }
+
+
+        [Display(Name = "Historias de usuario : ")]
+        public virtual ICollection<ClasesHistoriaUsuario.HistoriaUsuario> historiasUsuario { get; set; }
         
     }
 }
