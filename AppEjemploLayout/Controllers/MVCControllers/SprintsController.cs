2af718d3cf3686d61838d3ee869ff2c7561e3398
DanielFelippe <dfmarin@unicauca.edu.co>
2016-12-08 19:35:22 -0500

    se arreglo los links de eliminar sprint

--- AppEjemploLayout/Controllers/MVCControllers/SprintsController.cs
+++ AppEjemploLayout/Controllers/MVCControllers/SprintsController.cs
@@ -149,6 +149,7 @@ namespace AppEjemploLayout.Controllers.MVCControllers
             {
                 return HttpNotFound();
             }
+            ViewBag.proyectoId = sprint.ProyectoId;
             return View(sprint);
         }
 
@@ -164,7 +165,7 @@ namespace AppEjemploLayout.Controllers.MVCControllers
             Sprint sprint = db.Sprint.Find(id);
             db.Sprint.Remove(sprint);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("UsuariosProyecto","Proyectoes", new { IdProyecto = sprint.ProyectoId });
         }
 
         protected override void Dispose(bool disposing)
